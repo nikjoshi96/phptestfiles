409a94d7f35376beb9cff26ed1ca4a862ffbe5b0
Nikunj Joshi <nikunjjoshiphpdeveloper@gmail.com>
2025-09-19 10:58:14 +0400

    Update convert_timezone_utc_ist.php
    Updated Timezone current time to IST and Dubai Time

--- convert_timezone_utc_ist.php
+++ convert_timezone_utc_ist.php
@@ -1,12 +1,18 @@
 <?php
-	$timestamp = strtotime('2019-03-30T06:05:03+0000');
-	
+	//$timestamp = strtotime('2019-03-30T06:05:03+0000');
+	$timestamp = strtotime(date('Y-m-dTH:i:s').'+0000');
 	//echo date('Y-m-d h:i A', $timestamp).'<br />';
+
+	$dtime = new DateTime("Asia/Dubai");
+	$dtime->setTimestamp($timestamp);
+
+	$stringtime = $dtime->format('d-M-Y h:i A');
+	echo "Dubai Time: ".$stringtime."<br />";
 
 	$dtime = new DateTime("Asia/Kolkata");
 	$dtime->setTimestamp($timestamp);
 
 	$stringtime = $dtime->format('d-M-Y h:i A');
-	echo $stringtime;
+	echo "India Time: ".$stringtime."<br />";
 	
 ?>
